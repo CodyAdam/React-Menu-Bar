568bdd97b14bac20426b640cfeb4b359c9844a64
CodyAdam <cody.ad4@gmail.com>
2020-05-15 23:48:58 +0200

    Add build

---
+++ docs/precache-manifest.b9c0b669696c9b0d889c433289751c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f50d7ee09dea85e12bf27dbb747216",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b199244240b3c00804e3",
+    "url": "./static/css/main.61a6495d.chunk.css"
+  },
+  {
+    "revision": "2d276e1768c213669fbe",
+    "url": "./static/js/2.b52e5897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b52e5897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b199244240b3c00804e3",
+    "url": "./static/js/main.a3870cb1.chunk.js"
+  },
+  {
+    "revision": "5062a10d743dc7f4ab18",
+    "url": "./static/js/runtime-main.3888d738.js"
+  }
+]);
